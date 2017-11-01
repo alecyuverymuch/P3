ae959c9907482af6e37d3ed0edea983bab281e76
alecyuverymuch <amyu@wisc.edu>
2017-10-31 19:40:56 -0500

    Fix

--- files/ast.java
+++ files/ast.java
@@ -211,9 +211,8 @@ class StmtListNode extends ASTnode {
         Iterator it = myStmts.iterator();
         try {
             while (it.hasNext()) {
-                doIndent(p, indent);
+                //doIndent(p, indent);
                 ((StmtNode)it.next()).unparse(p, indent);
-                p.print("\n");
             }
         } catch (NoSuchElementException ex) {
             System.err.println("unexpected NoSuchElementException in StmtListNode.print");
